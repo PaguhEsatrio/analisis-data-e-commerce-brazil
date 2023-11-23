8e8ffa8f307298af143971d2a1ffdbcb96d2ca34
Paguh Esatrio <97024224+PaguhEsatrio@users.noreply.github.com>
2023-11-24 00:21:08 +0700

    Update app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -96,8 +96,9 @@ with col1:
 with col2:
     banyakStateKota_df = banyakStateKota(all_df)
     stateBanyakKota = banyakStateKota_df['jumlah'].max()
-    spillKota =  banyakStateKota_df.loc[banyakState_df['jumlah'].idxmax()]['customer_city']
-    st.markdown(f"Customer Paling Banyak Kota : **{stateBanyakKota}** (**{spillKota}**) ")
+    spillKota =  banyakStateKota_df.loc[banyakStateKota_df['jumlah'].idxmax()]['customer_city']
+    st.markdown(f"Customer Paling Banyak Kota: **{stateBanyakKota}** (**{spillKota}**)")
+
 
 fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(16, 8))
 
